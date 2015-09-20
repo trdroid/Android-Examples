fc23981d28ae910c284d2a2bbb4cb06b8b8fd1ad
konceptsandcode <koncepts.and.code@gmail.com>
2015-09-19 23:11:09 -0400

    canceling notification

--- Notifications/BasicNotification/app/src/main/java/konceptsandcode/com/basicnotification/NotificationBoard.java
+++ Notifications/BasicNotification/app/src/main/java/konceptsandcode/com/basicnotification/NotificationBoard.java
@@ -1,5 +1,7 @@
 package konceptsandcode.com.basicnotification;
 
+import android.app.NotificationManager;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +13,15 @@ public class NotificationBoard extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification_board);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        int notificationID = getIntent().getExtras().getInt("notificationID");
+
+        /*
+            Cancel the notification once the user taps on the notification from the notifications list.
+            Without this, the notification can still be seen in the status bar.
+         */
+        notificationManager.cancel(notificationID);
     }
 }
