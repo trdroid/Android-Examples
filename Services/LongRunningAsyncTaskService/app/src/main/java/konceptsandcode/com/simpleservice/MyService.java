e2841afb32595708f359b8e9aaf10633eb72266f
konceptsandcode <koncepts.and.code@gmail.com>
2015-09-23 20:05:53 -0400

    removing comments

--- Services/LongRunningAsyncTaskService/app/src/main/java/konceptsandcode/com/simpleservice/MyService.java
+++ Services/LongRunningAsyncTaskService/app/src/main/java/konceptsandcode/com/simpleservice/MyService.java
@@ -18,18 +18,12 @@ public class MyService extends Service
     {
     }
 
-    /*
-        onBind() allows to bind an activity to a service. This allows the activity to directly access members and methods of the service.
-     */
     @Override
     public IBinder onBind(Intent intent)
     {
         return null;
     }
 
-    /*
-        onStartCommand() is called when the service is explicitly started using the startService() method
-     */
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
@@ -47,11 +41,6 @@ public class MyService extends Service
         return START_STICKY;
     }
 
-    /*
-        onDestroy() is called when the service is stopped using the stopService() method.
-        A service can stop itself by calling stopSelf() method
-        This is where the resources used by the service should be freed.
-     */
     @Override
     public void onDestroy() {
         super.onDestroy();
