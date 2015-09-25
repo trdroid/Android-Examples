001a3438a2dbd989bb6fa753901987ed30c30044
konceptsandcode <koncepts.and.code@gmail.com>
2015-09-24 22:05:19 -0400

    IntentService example

---
+++ Services/SimpleIntentService/app/src/main/java/konceptsandcode/com/simpleservice/MainActivity.java
@@ -0,0 +1,20 @@
+package konceptsandcode.com.simpleservice;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onServiceStartButtonClick(View view) {
+        startService(new Intent(getBaseContext(), MyIntentService.class));
+    }
+}
---
+++ Services/SimpleIntentService/app/src/main/java/konceptsandcode/com/simpleservice/MyIntentService.java
@@ -0,0 +1,44 @@
+package konceptsandcode.com.simpleservice;
+
+import android.app.IntentService;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+/*
+    Intent Service uses a worker thread to run its task.
+    Unlike a Service, which has to be stopped explicitly, an IntentService terminates itself when the task completes.
+ */
+public class MyIntentService extends IntentService
+{
+    public MyIntentService()
+    {
+        /*
+            Assign a name for the intent service's worker thread; useful for debugging
+         */
+        super("MyIntentService");
+    }
+
+
+    @Override
+    protected void onHandleIntent(Intent intent)
+    {
+        /*
+            Toast.makeText(this, "Starting Task ...", Toast.LENGTH_LONG).show();
+
+            Since the code runs on a separate thread, UI cannot be accessed from onHandleIntent method
+         */
+
+        Log.d("MY_INTENT_SERVICE", "Starting Task ...");
+
+        try {
+            for(int iter = 0; iter < 5; iter++) {
+                Thread.sleep(1000);
+            }
+        }catch(InterruptedException e){}
+
+        Log.d("MY_INTENT_SERVICE", "Ending Task ...");
+    }
+}
