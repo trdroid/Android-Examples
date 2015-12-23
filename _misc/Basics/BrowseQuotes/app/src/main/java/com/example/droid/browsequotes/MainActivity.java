dabe6137f6d8ac86f9d55bc37f287ec5adf6d3fc
konceptsandcode <koncepts.and.code@gmail.com>
2015-12-23 12:30:20 -0500

    finishing up with code

--- _misc/Basics/BrowseQuotes/app/src/main/java/com/example/droid/browsequotes/MainActivity.java
+++ _misc/Basics/BrowseQuotes/app/src/main/java/com/example/droid/browsequotes/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,22 +13,40 @@ public class MainActivity extends AppCompatActivity {
      */
     private Button mPreviousButton;
     private Button mNextButton;
+    private TextView mQuoteTextView;
+
+    private int mQuoteIndex;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        QuoteModel[] quotes = QuotesCollection.getQuotes();
+        final QuoteModel[] quotes = QuotesCollection.getQuotes();
 
         mPreviousButton = (Button) findViewById(R.id.previous_button);
         mNextButton = (Button) findViewById(R.id.next_button);
+        mQuoteTextView = (TextView) findViewById(R.id.quote_text_view);
 
         mPreviousButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                mQuoteIndex = mQuoteIndex > 0 ? --mQuoteIndex : quotes.length - 1;
+                setQuoteText(mQuoteTextView, quotes[mQuoteIndex].getQuoteId());
             }
         });
+
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mQuoteIndex = ++mQuoteIndex % quotes.length;
+                setQuoteText(mQuoteTextView, quotes[mQuoteIndex].getQuoteId());
+            }
+        });
+    }
+
+    private void setQuoteText(TextView quoteTextView, int quoteID) {
+        quoteTextView.setText(quoteID);
     }
 }
--- _misc/Basics/BrowseQuotes/app/src/main/java/com/example/droid/browsequotes/QuotesCollection.java
+++ _misc/Basics/BrowseQuotes/app/src/main/java/com/example/droid/browsequotes/QuotesCollection.java
@@ -4,7 +4,8 @@ public class QuotesCollection {
     public static QuoteModel[] getQuotes() {
         QuoteModel[] quotes = new QuoteModel[] {new QuoteModel(R.string.quote1),
                 new QuoteModel(R.string.quote2),
-                new QuoteModel(R.string.quote3)
+                new QuoteModel(R.string.quote3),
+                new QuoteModel(R.string.quote4)
         };
 
         return quotes;
