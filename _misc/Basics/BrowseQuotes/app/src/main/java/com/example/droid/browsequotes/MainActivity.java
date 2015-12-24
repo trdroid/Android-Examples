394b92718460fbbc64a15ecad7c87b2ab977dbdd
konceptsandcode <koncepts.and.code@gmail.com>
2015-12-23 23:44:25 -0500

    few changes

--- _misc/Basics/BrowseQuotes/app/src/main/java/com/example/droid/browsequotes/MainActivity.java
+++ _misc/Basics/BrowseQuotes/app/src/main/java/com/example/droid/browsequotes/MainActivity.java
@@ -17,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
 
     private int mQuoteIndex;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
