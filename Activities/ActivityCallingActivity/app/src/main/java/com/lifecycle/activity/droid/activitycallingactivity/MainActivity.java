b8dff97830e966e77752ac65e74b2158b675e8f4
konceptsandcode <gruprog@gmail.com>
2016-01-16 20:12:13 -0500

    wrapping up

--- Activities/ActivityCallingActivity/app/src/main/java/com/lifecycle/activity/droid/activitycallingactivity/MainActivity.java
+++ Activities/ActivityCallingActivity/app/src/main/java/com/lifecycle/activity/droid/activitycallingactivity/MainActivity.java
@@ -3,10 +3,14 @@ package com.lifecycle.activity.droid.activitycallingactivity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
                     In contrast, an Implicit intent is used to start an Activity in another application
                  */
                 Intent startSecondActivityIntent = new Intent(MainActivity.this, SecondActivity.class);
+                startSecondActivityIntent.putExtra(SecondActivity.EXTRA_MESSAGE, R.string.message1);
+                startSecondActivityIntent.putExtra(SecondActivity.INFORM_MESSAGE, R.string.inform_message);
 
                 /*
                     When startActivity(intent) is called, the call is sent to the ActivityManager of Android OS.
@@ -39,7 +45,88 @@ public class MainActivity extends AppCompatActivity {
                      and calls its onCreate() method
                  */
                 startActivity(startSecondActivityIntent);
+
+                Log.d(TAG, "startActivity() is a synchronous call");
             }
         });
+
+        Button launchButtonForResult = (Button)findViewById(R.id.launch_button_for_result);
+
+        launchButtonForResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startSecondActivityForResultIntent = new Intent(MainActivity.this, SecondActivity.class);
+
+                /*
+                    For the key in putExtra() method, the convention is to use a constant from the Child Activity class
+                    which receives and processes the data
+
+                    Question
+                    ~~~~~~~~~~~~~~~~~~
+
+                    What if an Explicit Intent is used to invoke an Activity from another application package?
+
+                    Say, if MainActivity invokes an Activity called ExternalActivity from a different application package
+                    In that case, how can a constant defined in ExternalActivity be accessed? for eg. ExternalActivity.EXTRA_MESSAGE
+                    defined in ExternalActivity
+                 */
+                startSecondActivityForResultIntent.putExtra(SecondActivity.EXTRA_MESSAGE, R.string.message2);
+                startSecondActivityForResultIntent.putExtra(SecondActivity.INFORM_MESSAGE, R.string.inform_message);
+
+                /*
+                    startActivityForResult(intent, requestCode)
+
+                    is used when the Parent Activity expects the Child Activity to send it results,
+                    which is handled by
+
+                        protected void onActivityResult(int requestCode, int resultCode, Intent data)
+
+                    callback defined below
+
+                    The requestCode is sent to the child activity which returns it back to the parent Activity
+                        when sending the results
+
+                    It is used when the parent Activity starts more than one Child Activity to
+                        identify which child activity is responding with the results
+                 */
+                startActivityForResult(startSecondActivityForResultIntent, 0);
+
+                Log.d(TAG, "startActivityForResult() is a synchronous call");
+            }
+        });
+    }
+
+    /*
+        When a Child Activity started with startActivityForResult() method returns the results, the following callback method,
+          onActivityResult() is called
+
+        The requestCode is a custom identification number sent to the Child Activity at the time of starting it
+            with startActivityForResult() method
+
+        The resultCode is sent back to the Parent Activity when the Child Activity responds with a result.
+
+        It is used to by the Parent Activity to identify which Child Activity responded with the result.
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        /*
+            Question
+            ~~~~~~~~
+
+            What is the best convention to define requestCodes?
+
+            Where? as constants in the same file, or a separate file or enums?
+         */
+
+        if(data == null) {
+            return;
+        }
+
+        switch(requestCode) {
+            case 0:
+                int message = data.getIntExtra(SecondActivity.RESULT, -1);
+                Toast.makeText(getApplicationContext(), String.valueOf(message), Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- Activities/ActivityCallingActivity/app/src/main/java/com/lifecycle/activity/droid/activitycallingactivity/SecondActivity.java
+++ Activities/ActivityCallingActivity/app/src/main/java/com/lifecycle/activity/droid/activitycallingactivity/SecondActivity.java
@@ -1,13 +1,93 @@
 package com.lifecycle.activity.droid.activitycallingactivity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
+    /*
+        Since any Activity can start SecondActivity, the convention is to define the keys for EXTRAS in the
+        Activity that retrieves and processes the attached data, which is SecondActivity
+
+        Therefore, by convention, define the following String constant, that can be used by the Activities
+        calling SecondActivity when sending data through the intent
+
+        This applies to both the incoming and the outgoing EXTRAS which act as an "interface" for the SecondActivity
+
+        To prevent name collisions with extras from other apps, add the package name as a prefix to the constant
+     */
+    public static final String EXTRA_MESSAGE = "com.lifecycle.activity.droid.activitycallingactivity.message";
+    public static final String INFORM_MESSAGE = "com.lifecycle.activity.droid.activitycallingactivity.inform";
+
+    public static final String RESULT = "com.lifecycle.activity.droid.activitycallingactivity.result";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        /*
+            getIntent() returns the Intent object sent from startActivity(intent)
+            getXXXExtra() method returns the data attached to the intent
+         */
+        int message1ResourceId = getIntent().getIntExtra(EXTRA_MESSAGE, -1);
+        int message2ResourceId = getIntent().getIntExtra(INFORM_MESSAGE, -1);
+
+        /*
+            Get the resource name from the resource id
+            ------------------------------------------
+
+            String message1 = getResources().getResourceEntryName(message1ResourceId);
+            String message2 = getResources().getResourceEntryName(message2ResourceId);
+         */
+
+        String message1 = getResources().getString(message1ResourceId);
+        String message2 = getResources().getString(message2ResourceId);
+
+        Toast.makeText(getApplicationContext(), message1 + "\n" + message2, Toast.LENGTH_LONG).show();
+
+        Button closeButton = (Button)findViewById(R.id.close_button);
+
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*
+                    'this' in onClick() method refers to an instance of type View.OnClickListener, but not of type SecondActivity
+                    To refer to outer this, use SecondActivity.this
+                 */
+
+                /*
+                    calling finish() is similar to hitting the back button which pops the instance of
+                    SecondActivity from the Activity stack
+                 */
+                SecondActivity.this.finish();
+            }
+        });
+
+        Button returnResultButton = (Button)findViewById(R.id.return_result_button);
+
+        returnResultButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent result = new Intent();
+
+                result.putExtra(RESULT, 10);
+
+                /*
+                    setResult() method can be used in the Child Activity to send data back to the Parent Activity.
+                      After the Child Activity is destroyed (say, after pressing the back button) the ActivityManager calls the
+                      onActivityResult(int requestCode, int resultCode, Intent data) method of the parent Activity
+
+                     setResult(int resultCode)
+
+                     Intent implies the data that would be passed to the Parent Activity
+                     setResult(int resultCode, Intent data)
+                 */
+                setResult(RESULT_OK, result);
+            }
+        });
     }
 }
