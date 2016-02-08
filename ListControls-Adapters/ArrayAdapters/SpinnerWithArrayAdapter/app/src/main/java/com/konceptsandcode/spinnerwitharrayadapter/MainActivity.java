6a78bcbb2bfeb21f56f3cee422dc9b3a9f90d996
konceptsandcode <gruprog@gmail.com>
2016-02-08 02:48:05 -0500

    spinner example

---
+++ ListControls-Adapters/ArrayAdapters/SpinnerWithArrayAdapter/app/src/main/java/com/konceptsandcode/spinnerwitharrayadapter/MainActivity.java
@@ -0,0 +1,23 @@
+package com.konceptsandcode.spinnerwitharrayadapter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner_view);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.cars, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+    }
+}
