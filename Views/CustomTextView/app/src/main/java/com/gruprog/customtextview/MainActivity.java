c02040cd001dc5f4c0b6592d6dd40050c6a50f75
trdroid <terrific.droid.prog@gmail.com>
2016-08-20 10:09:16 -0400

    custom text view project

---
+++ Views/CustomTextView/app/src/main/java/com/gruprog/customtextview/MainActivity.java
@@ -0,0 +1,15 @@
+package com.gruprog.customtextview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        CustomTextView customTextView = new CustomTextView(this);
+        customTextView.setText("Hello World!");
+        setContentView(customTextView);
+    }
+}
